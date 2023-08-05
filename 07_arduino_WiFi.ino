7082074d75776b5dae2e785a3a15fa2bb1edd6c6
goodlucky-Joy <goodlucky99@gmail.com>
2023-08-05 20:04:43 +0900

    Create 07_arduino_WiFi.ino

---
+++ 07_arduino_WiFi.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+
+/** 인터넷 공유기의 WiFi 이름과 비밀번호를 입력합니다 */
+const char* ssid = "mySSID";
+const char* password = "myPW";
+
+void setup() {
+  Serial.begin(9600);
+ 
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  // 해당 공유기에 접속 시도
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nWiFi Connected");
+  
+  // 공유기로부터 할당 받은 "(사설)IP 주소" 출력
+  Serial.print("Local IP address : ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}
