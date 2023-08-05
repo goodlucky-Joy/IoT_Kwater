fe02285da5462213d9eed4fbd69bcabba387ae2c
goodlucky-Joy <goodlucky99@gmail.com>
2023-08-05 22:47:10 +0900

    Update 41_Python_MQTT_ClientSub.py

--- 41_Python_MQTT_ClientSub.py
+++ 41_Python_MQTT_ClientSub.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 def on_connect( client, userdata, flags, rc ):
    print("Connect with result code " + str(rc) )
    client.subscribe("temp")
+   #client.subscribe("/kwater/kwater01/#")
 
 def on_message( client, userdata, msg ):
    print( msg.topic +" "+str(msg.payload) )
