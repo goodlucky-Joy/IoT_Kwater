d47c0820b00e76aeedae280798ef920c20663f66
goodlucky-Joy <goodlucky99@gmail.com>
2023-08-06 03:52:28 +0900

    Update 52_Python_InfluxDBClient.py

--- 52_Python_InfluxDBClient.py
+++ 52_Python_InfluxDBClient.py
@@ -17,10 +17,3 @@ point = Point("home").tag("room", "kitchen").field("temp", 25.5).field("hum", 60
 # 데이터 쓰기
 write_api = client.write_api(write_options=SYNCHRONOUS)
 write_api.write(bucket=bucket, org=org, record=point)
-
-# 데이터 읽기
-query = f'from(bucket: "{bucket}") |> range(start: -1h)'
-tables = client.query_api().query(query, org=org)
-for table in tables:
-    for row in table.records:
-        print(f'Time: {row["_time"]}, Location: {row["location"]}, Temperature: {row["_value"]}')
