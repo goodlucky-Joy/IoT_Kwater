8cf975f97a1328e9c82d79bc998f655e5e38f88f
goodlucky-Joy <goodlucky99@gmail.com>
2023-08-06 03:57:55 +0900

    Update 52_Python_InfluxDBClient.py

--- 52_Python_InfluxDBClient.py
+++ 52_Python_InfluxDBClient.py
@@ -1,4 +1,4 @@
-from influxdb_client import InfluxDBClient, Point
+import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 
@@ -9,11 +9,13 @@ org = "DB_ORG"
 bucket = "DB_BUCKET"
 
 # InfluxDB 클라이언트 생성
-client = InfluxDBClient(url=url, token=token)
+client = InfluxDBClient(url=url, token=token, org=org)
+
+# write_api 인스턴스 생성
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # 데이터 포인트 생성
-point = Point("home").tag("room", "kitchen").field("temp", 25.5).field("hum", 60).field("co",3i).time(datetime.utcnow())
+point = Point("home").tag("room", "kitchen").field("temp", 25.5).field("hum", 60).field("co",3).time(datetime.utcnow())
 
 # 데이터 쓰기
-write_api = client.write_api(write_options=SYNCHRONOUS)
 write_api.write(bucket=bucket, org=org, record=point)
